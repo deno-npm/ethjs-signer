30deec471b301fb148d6cd93fdb4acac9cda2e57
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-06 21:48:39 -0500

    0.1.1

---
+++ deps.js
@@ -0,0 +1,8 @@
+export { default as Buffer } from "https://cdn.skypack.dev/buffer@5.6.0";
+export { default as jssha3 } from "https://cdn.skypack.dev/js-sha3@0.5.5";
+export { default as elliptic } from "https://cdn.skypack.dev/elliptic@6.5.3";
+export { default as BN } from "https://cdn.skypack.dev/bn.js@5.1.3";
+export * as ethjs_account from "https://deno.land/x/npm_ethjs_account@0.1.5/mod.js";
+export * as rlp from "https://deno.land/x/npm_rlp@2.2.6/mod.ts";
+export { default as numberToBN } from "https://cdn.skypack.dev/number-to-bn@1.7.0";
+export * as testing from "https://deno.land/std@0.68.0/testing/asserts.ts";
